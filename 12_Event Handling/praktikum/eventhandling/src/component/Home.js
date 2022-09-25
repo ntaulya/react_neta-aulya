7840aca07cbba71e7dc1eb3ece12d682bd070b2b
ntaulya <netaaulya@gmail.com>
2022-09-25 12:38:39 +0700

    benerin tugas 13

--- 12_Event Handling/praktikum/eventhandling/src/component/Home.js
+++ 12_Event Handling/praktikum/eventhandling/src/component/Home.js
@@ -8,12 +8,12 @@ class Home extends React.Component {
             data : [
                 {
                     id: 1,
-                    name: "Mengerjakan Excercise",
+                    title: "Mengerjakan Excercise",
                     completed: true,
                 },
                 {
                     id: 2,
-                    name: "Mengerjakan Assignment",
+                    title: "Mengerjakan Assignment",
                     completed: false,
                 }
             ]
@@ -22,22 +22,21 @@ class Home extends React.Component {
 
     hapusTask = (id) => {
         const newTask = this.state.data.filter((item) => item.id !== id)
-
+        console.log(id);
         this.setState ({data : newTask})
     }
 
     tambahTask = (title) => {
         let id = this.state.data[this.state.data.length-1].id + 1
         let completed = false;
-        const newTodo = {id, title, completed}
-        console.log(newTodo)
-        // this.setState({data: [...this.state.data, newTodo]})
+        const newTodo = {id, ...title, completed}
+        this.setState({data: [...this.state.data, newTodo]})
     }
 
     render () {
         return (
             <div>
-                <ListTask data={this.state.data}/>
+                <ListTask data={this.state.data} tambahTask={this.tambahTask} hapusTask={this.hapusTask}/>
             </div>
         )
     }
--- 12_Event Handling/praktikum/eventhandling/src/component/ListTask.js
+++ 12_Event Handling/praktikum/eventhandling/src/component/ListTask.js
@@ -1,34 +1,49 @@
+import React, {useState} from "react";
+
 const ListTask = (props) => {
-    const {data} = props
+    const {data, hapusTask} = props
+
+    const [state, setState] = useState([{
+        title: ""
+    }])
+
+    const onChange = (e) => {
+        setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const submitTask = (e) => {
+        e.preventDefault()
+        if (state.title) {
+            props.tambahTask({title:state.title})
+            setState({
+                title:""
+            })
+        }
+        else {
+            alert("tidak terinput")
+        }
+    }
     
+
     return (
         <div class="container">
-            <h5 class="title text-center">todos</h5>
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Add todo..." />
-                    <button class="input-group-text" id="basic-addon2">Submit</button>
+            <h5 className="title text-center">todos</h5>
+            <div className="input-group mb-3">
+                <input type="text" onChange = {onChange} name="title" value={state.title} className="form-control" placeholder="Add todo..." />
+                    <button className="input-group-text" id="basic-addon2" onClick={submitTask}>Submit</button>
             </div>
 
-            <div class="input-group mb-3">
-                <div class="input-group-text">
-                    <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" />
-                </div>  
-                { data.map ((ListTask => 
-                    
-                    <p key={ListTask.id} className={ListTask.completed? 'not-completed' : 'completed'}>{ListTask.name}</p>
-                ))}
-                <button class="input-group-text" id="basic-addon2">Delete</button>
-            </div>
-
-            <div class="input-group mb-3">
-                <div class="input-group-text">
-                    <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" />
+            { data.map ((ListTask => 
+                <div className="input-group mb-3">
+                    <div className="input-group-text">
+                        <input className="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" />
+                    </div>  
+                    <p key={ListTask.id} className={ListTask.completed? 'not-completed' : 'completed'}>{ListTask.title}</p>
+                    <button className="input-group-text" id="basic-addon2" onClick={()=>{hapusTask(ListTask.id)}}>Delete</button>
                 </div>
-                { data.map ((ListTask => 
-                    <p key={ListTask.id} className={ListTask.completed? 'not-completed' : 'completed'}>{ListTask.name}</p>
-                ))}
-                <button class="input-group-text" id="basic-addon2">Delete</button>
-            </div>
+            ))}
         </div>
     )
 }
